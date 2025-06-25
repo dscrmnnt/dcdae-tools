fc7b705eef188ac2f80c2d21a4720a7debc70739
Document <137008273+documentbit@users.noreply.github.com>
2025-06-25 23:24:25 +0300

    добавил рубиновый блок

--- src/main/java/dcdae/dcdaetools/DcdaeTools.java
+++ src/main/java/dcdae/dcdaetools/DcdaeTools.java
@@ -1,5 +1,6 @@
 package dcdae.dcdaetools;
 
+import dcdae.dcdaetools.block.ModBlocks;
 import dcdae.dcdaetools.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class DcdaeTools implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.register();
+		ModBlocks.register();
 	}
 }
